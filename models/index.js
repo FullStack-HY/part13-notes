ae13f37ca629e248c41f6918894e3c6564ed44ff
Matti Luukkainen <mluukkai@iki.fi>
2021-10-11 17:20:59 +0300

    part13-6

--- models/index.js
+++ models/index.js
@@ -4,9 +4,6 @@ const User = require('./user')
 User.hasMany(Note)
 Note.belongsTo(User)
 
-Note.sync({ alter: true })
-User.sync({ alter: true })
-
 module.exports = {
   Note, User
 }
--- util/db.js
+++ util/db.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize')
+const Umzug = require('umzug')
 const { DATABASE_URL } = require('./config')
 
 const sequelize = new Sequelize(DATABASE_URL, {
@@ -8,11 +9,31 @@ const sequelize = new Sequelize(DATABASE_URL, {
       rejectUnauthorized: false
     }
   },
-});
+})
+
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    storage: 'sequelize',
+    storageOptions: {
+      sequelize,
+      tableName: 'migrations',
+    },
+    migrations: {
+      params: [sequelize.getQueryInterface()],
+      path: `${process.cwd()}/migrations`,
+      pattern: /\.js$/,
+    },
+  })
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.file),
+  })
+}
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     console.log('database connected')
   } catch (err) {
     console.log('connecting database failed')
