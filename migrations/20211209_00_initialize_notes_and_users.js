33e285fac36d4b5a23b9a826b59e1207158087ab
Matti Luukkainen <mluukkai@iki.fi>
2021-10-11 17:28:35 +0300

    Create 20211209_00_initialize_notes_and_users.js

---
+++ migrations/20211209_00_initialize_notes_and_users.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.createTable('notes', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      important: {
+        type: DataTypes.BOOLEAN
+      },
+      date: {
+        type: DataTypes.DATE
+      },
+    })
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    })
+    await queryInterface.addColumn('notes', 'user_id', {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('notes')
+    await queryInterface.dropTable('users')
+  },
+}
