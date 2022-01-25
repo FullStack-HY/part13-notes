310bee80f894ab5fe9331c779e4fdc6231038806
Kalle Ilves <kalleilv@gmail.com>
2022-01-25 11:48:29 +0200

    umzug 3

--- util/db.js
+++ util/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const Umzug = require('umzug')
+const { Umzug, SequelizeStorage } = require('umzug')
 const { DATABASE_URL } = require('./config')
 
 const sequelize = new Sequelize(DATABASE_URL, {
@@ -11,22 +11,22 @@ const sequelize = new Sequelize(DATABASE_URL, {
   },
 })
 
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+
 const runMigrations = async () => {
-  const migrator = new Umzug({
-    storage: 'sequelize',
-    storageOptions: {
-      sequelize,
-      tableName: 'migrations',
-    },
-    migrations: {
-      params: [sequelize.getQueryInterface()],
-      path: `${process.cwd()}/migrations`,
-      pattern: /\.js$/,
-    },
-  })
+  const migrator = new Umzug(migrationConf)
   const migrations = await migrator.up()
+
   console.log('Migrations up to date', {
-    files: migrations.map((mig) => mig.file),
+    files: migrations.map((mig) => mig.name),
   })
 }
 
@@ -36,7 +36,7 @@ const connectToDatabase = async () => {
     await runMigrations()
     console.log('database connected')
   } catch (err) {
-    console.log('connecting database failed')
+    console.log('connecting database failed', err)
     return process.exit(1)
   }
 
